d99dc5461678c1e0ea3ddd6ecc155a36ae3facda
= <=>
2024-01-19 16:21:31 -0300

    cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import routes from "./routes.js";
-
+import cors from "cors";
 const app = express();
-
+app.use(cors(
+{
+origin: '*',
+methods: ['GET', 'POST', 'PUT', 'DELETE'],
+allowedHeaders: ['Content-Type', 'Authorization']
+}
+));
 app.use(express.json());
 
 //use routes

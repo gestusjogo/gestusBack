c0282f549cc21c921473ef023bff469b7cf89292
= <=>
2024-01-20 20:31:47 -0300

    update fases concluidas

--- src/app/controllers/FasesConcluidasController.js
+++ src/app/controllers/FasesConcluidasController.js
@@ -2,25 +2,36 @@ import FasesConcluidas from "../model/FasesConcluidas.js";
 import FasesConcluidasRepository from "../repositories/FasesConcluidasRepository.js";
 
 class FasesConcluidasController {
-  async create(request, response) {
+  async inicio(request, response) {
     try {
-      const fasesConcluidas = new FasesConcluidas(
-        request.body.idUsuario,
-        request.body.inicio,
-        request.body.fliperama,
-        request.body.escola,
-        request.body.supermercado,
-        request.body.ambiental,
-        request.body.sorveteria,
-        request.body.praca
-      );
-      await FasesConcluidasRepository.create(fasesConcluidas);
-      response.json({ message: "Success" });
+      const idUsuario = request.body.id_usuario;
+      const fasesConcluidas = new FasesConcluidas(idUsuario, true, false, false, false, false, false, false);
+      const insertedId = await FasesConcluidasRepository.create(fasesConcluidas);
+      response.json({ id: insertedId });
     } catch (error) {
       response.status(500).json({ error: "Internal Server Error" });
     }
   }
 
+  async atualizacao(request, response) {
+    try {
+      const idUsuario = request.body.id_usuario;
+      const fase = request.body.fase;
+      const fasesConcluidas = await FasesConcluidasRepository.findById(idUsuario);
+      
+      if (!fasesConcluidas) {
+        response.status(404).json({ message: "Fases Concluidas not found" });
+      } else {
+        // Atualize a fase específica para '1' (concluída)
+        fasesConcluidas[fase] = true;
+        await FasesConcluidasRepository.update(idUsuario, fasesConcluidas);
+        response.json({ message: "Success" });
+      }
+    } catch (error) {
+      response.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  
   async findAll(request, response) {
     try {
       const fasesConcluidas = await FasesConcluidasRepository.findAll();
--- src/app/routes.js
+++ src/app/routes.js
@@ -7,15 +7,9 @@ const router = Router();
 
 router.post('/usuarios/single', UsuarioController.createSingle);
 router.post('/usuarios/multiplo', UsuarioController.createMulti);
-router.get('/usuarios/:id', UsuarioController.findById);
 router.get('/usuarios', UsuarioController.findAll);
-router.put('/usuarios/:id', UsuarioController.updateById);
-router.delete('/usuarios/:id', UsuarioController.deleteById);
-router.post("/fases_concluidas", FasesConcluidasController.create);
-router.get("/fases_concluidas", FasesConcluidasController.findAll);
-router.get("/fases_concluidas/:id", FasesConcluidasController.findById);
-router.put("/fases_concluidas/:id", FasesConcluidasController.updateById);
-router.delete("/fases_concluidas/:id", FasesConcluidasController.deleteById);
+router.post("/fases_concluidas", FasesConcluidasController.inicio);
+router.post("/fases_concluidas/:id", FasesConcluidasController.atualizacao);
 router.post("/avaliacao", AvaliacaoController.create);
 router.get("/avaliacao", AvaliacaoController.findAll);
 router.get("/avaliacao/:id", AvaliacaoController.findById);
